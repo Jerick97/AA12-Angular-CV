a19c550f9dedd0636b32e63d4d1580428bed3522
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-05 10:17:47 -0500

    Agregada Sección Información

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { InformationComponent } from './information/information.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    InformationComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    InformationComponent
   ]
 })
 export class PagesModule { }
